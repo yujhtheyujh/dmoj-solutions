640e3a1aa272d36eac515b8a8a7cf92037544606
yujh-yujh <63082800+yujh-yujh@users.noreply.github.com>
2025-06-15 22:50:20 -0400

    Update hkccc15s2.py

--- hkccc15s2.py
+++ hkccc15s2.py
@@ -1,16 +1,16 @@
 import math
-n, m = map(int, input().split())
-s = input()
-t = input()
-g = math.gcd(len(s), len(t))
-list1 = [[0] * g for _ in range(26)]
-list2 = [[0] * g for _ in range(26)]
-for i in range(len(s)):
-    list1[ord(s[i]) - 97][i % g] += 1
-for i in range(len(t)):
-    list2[ord(t[i]) - 97][i % g] += 1
-su = 0
+N,M=map(int,input().split())
+s=input()
+t=input()
+g=math.gcd(len(s),len(t))
+S=0
 for i in range(g):
+    l=[0]*26
+    L=[0]*26
+    for j in s[i::g]:
+        l[ord(j)-97]+=1
+    for j in t[i::g]:
+        L[ord(j)-97]+=1
     for j in range(26):
-        su += list1[j][i] * list2[j][i]
-print(su * n * len(s) // math.lcm(len(s), len(t)))
+        S+=l[j]*L[j]
+print(S*len(s)*N//math.lcm(len(s),len(t)))
